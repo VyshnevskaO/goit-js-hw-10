61266801e8761e75e5a63923934b4d047f12cf6e
VyshnevskaO <vyshnevskaolenka@gmail.com>
2024-04-06 00:14:53 +0200

    first task done

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,25 +1,26 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css"
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+// Number of milliseconds per unit of time
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
 
 const startBtn = document.querySelector(".timer-btn");
 const dateInput = document.querySelector("#datetime-picker");
 const display = document.querySelectorAll(".value");
-const transformedDisplay = [...display];
-
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-
-startBtn.addEventListener("click", onStartClick);
-
 
 let userSelectedDate;
 let msSelected;
 let dateObj = {};
 let delta;
+let intervalID;
+
+startBtn.addEventListener("click", onStartClick);
+startBtn.disabled = true;
 
 const options = {
   enableTime: true,
@@ -28,20 +29,27 @@ const options = {
   minuteIncrement: 1,
     onClose(selectedDates) {
         if (selectedDates[0] < new Date()) {
-            startBtn.disabled = true;
-            alert("Please choose a date in the future");
-           
+          startBtn.disabled = true;
+          iziToast.show({ message:'Please choose a date in the future'});
         } else {
-            startBtn.disabled = false;
+          startBtn.disabled = false;
             userSelectedDate = selectedDates[0];
             msSelected = userSelectedDate.getTime();
       }
   },
-}; 
+}
 
 flatpickr(dateInput, options); 
 
+function addLeadingZero({ days, hours, minutes, seconds }) {
 
+  days = days.toString().padStart(2, "0");
+  hours = hours.toString().padStart(2, "0");
+  minutes = minutes.toString().padStart(2, "0");
+  seconds = seconds.toString().padStart(2, "0");
+    
+  return dateObj = { days, hours, minutes, seconds };
+}
 
 function convertMs(ms) {
   // Remaining days
@@ -53,36 +61,33 @@ function convertMs(ms) {
   // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-  dateObj = { days, hours, minutes, seconds };
-    
+  return dateObj = { days, hours, minutes, seconds };
+  
 }
-
-// function addLeadingZero({ days, hours, minutes, seconds }) {
-//     const date = new Date();
-//     dateObj.daysLeft = date.getDate() - days;
-//     dateObj.hoursLeft = date.getHours() - hours;
-//     dateObj.minutesLeft = date.getMinutes() - minutes;
-//     dateObj.secondsLeft = date.getSeconds() - seconds;
-//      dateObj = { daysLeft, hoursLeft, minutesLeft, secondsLeft };
-// }
-
-
 
 function onStartClick() {
 
-     setInterval(() => {
-        
-            const date = new Date();
-            const dataMs = date.getTime();
-            delta = Math.abs(dataMs - msSelected);
-            
-        convertMs(delta);
-        // addLeadingZero(dateObj)
-        display[0].textContent = `${dateObj.days}`;
-        display[1].textContent = `${dateObj.hours}`;
-        display[2].textContent = `${dateObj.minutes}`;
-        display[3].textContent = `${dateObj.seconds}`;
-    }, 1000)
+  intervalID = setInterval(() => {
+    
+    const date = new Date();
+    const dataMs = date.getTime();
+    delta = Math.abs(dataMs - msSelected);
+
+    if ((dataMs - msSelected)> 0) {
+      clearInterval(intervalID);
+      return
+    } else {
+      startBtn.disabled = true;
+      dateInput.disabled = true;
+      convertMs(delta);
+      addLeadingZero(dateObj);
+
+      display[0].textContent = `${dateObj.days}`;
+      display[1].textContent = `${dateObj.hours}`;
+      display[2].textContent = `${dateObj.minutes}`;
+      display[3].textContent = `${dateObj.seconds}`;
+    }
+  }, 1000);
 
 }
 
